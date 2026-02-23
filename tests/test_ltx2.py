919cdb171fc564e93f1c9c62155c9d32e6591ff3
linnan wang <linnanw@nvidia.com>
2026-02-23 12:25:05 +0800

    add fastgen header

--- tests/test_ltx2.py
+++ tests/test_ltx2.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 import torch
 import numpy as np
 from diffusers.pipelines.ltx2.export_utils import encode_video
